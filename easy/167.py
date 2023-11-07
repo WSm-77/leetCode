9364ef06c4a8fd27361735b6953ac647619fb171
wiktor <wiktor.sedzimir@gmail.com>
2023-11-07 19:23:09 +0100

    Added second solution 167.py

--- easy/167.py
+++ easy/167.py
@@ -1,5 +1,9 @@
 from random import randint
 
+##################
+# first solution #
+##################
+
 def sum2(numArray, targetSum):
     arrayLen = len(numArray)
     windoWStart = 0
@@ -21,6 +25,25 @@ def sum2(numArray, targetSum):
 
 
     return windoWStart + 1, windowEnd + 1
+
+###################
+# second solution #
+###################
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        tabLen = len(numbers)
+        firstIndex = 0
+        secondIndex = tabLen - 1
+
+        while secondIndex > firstIndex:
+            currentSum = numbers[firstIndex] + numbers[secondIndex]
+            if currentSum == target:
+                return [firstIndex + 1, secondIndex + 1]
+            elif currentSum > target:
+                secondIndex -= 1
+            else:
+                firstIndex += 1
 
 if __name__ == "__main__":
     numArray = [randint(0, 15) for _ in range(10)]
