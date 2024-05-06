55a41da9fee008f24acbd690d3731c0c2c8a0032
wiktor <wiktor.sedzimir@gmail.com>
2024-05-06 20:21:05 +0200

    Add 332.py

---
+++ hard/332.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+        def dfs_visit(vertex: str):
+            nonlocal graph, itinerary, itineraryIdx
+
+            if vertex in graph:
+                for neighbour in sorted(graph[vertex], key=lambda x: x[0]):
+                    if not neighbour[1]:
+                        neighbour[1] = True
+                        dfs_visit(neighbour[0])
+
+            itinerary[itineraryIdx] = vertex
+            itineraryIdx -= 1
+        #end def
+        
+        E = len(tickets)
+        graph: dict[str, list[list[int | bool]]] = {}
+
+        for vertex, neighbour in tickets:
+            if vertex in graph:
+                graph[vertex].append([neighbour, False])
+            else:
+                graph[vertex] = [[neighbour, False]]
+
+        itinerary = [None for _ in range(E + 1)]
+        itineraryIdx = E
+
+        dfs_visit("JFK")
+
+        return itinerary
+        
+
+if __name__ == "__main__":
+    test = Solution()
+    tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    print(test.findItinerary(tickets))
+    tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+    print(test.findItinerary(tickets))
+
